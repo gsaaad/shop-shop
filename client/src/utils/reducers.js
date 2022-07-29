5ea3eb418f2a613c1cf0b41d6560ad0fe939337f
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-07-29 15:16:50 -0400

    added cart and finalized tests and reducers

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -2,6 +2,12 @@ import {
   UPDATE_PRODUCTS,
   UPDATE_CATEGORIES,
   UPDATE_CURRENT_CATEGORY,
+  ADD_TO_CART,
+  ADD_MULTIPLE_TO_CART,
+  REMOVE_FROM_CART,
+  UPDATE_CART_QUANTITY,
+  CLEAR_CART,
+  TOGGLE_CART,
 } from "./actions";
 
 // reducer frmo react
@@ -26,6 +32,55 @@ export const reducer = (state, action) => {
       return {
         ...state,
         currentCategory: action.currentCategory,
+      };
+    // if action type value is ADD_TO_CART, return a new state object with added product to cart array
+    case ADD_TO_CART:
+      return {
+        ...state,
+        cartOpen: true,
+        cart: [...state.cart, action.product],
+      };
+    // if action type value is ADD_MULTIPLE_TO_CART, return a new state object with multple products added to cart array
+    case ADD_MULTIPLE_TO_CART:
+      return {
+        ...state,
+        cart: [...state.cart, ...action.products],
+      };
+    // if action type value is REMOVE_FROM_CART, return a new state object with remove product from cart
+    case REMOVE_FROM_CART:
+      let newState = state.cart.filter((product) => {
+        return product._id !== action._id;
+      });
+
+      return {
+        ...state,
+        cartOpen: newState.length > 0,
+        cart: newState,
+      };
+    // if action type value is UPDATE_CART_QUANTITY, return a new state object with updated cart quantitty
+    case UPDATE_CART_QUANTITY:
+      return {
+        ...state,
+        cartOpen: true,
+        cart: state.cart.map((product) => {
+          if (action._id === product._id) {
+            product.purchaseQuantity = action.purchaseQuantity;
+          }
+          return product;
+        }),
+      };
+    // if action type value is CLEAR_CART, return a new state object with cleared cart array
+    case CLEAR_CART:
+      return {
+        ...state,
+        cartOpen: false,
+        cart: [],
+      };
+    // if action type value is TOGGLE_CART, return a new state object with toggled cart
+    case TOGGLE_CART:
+      return {
+        ...state,
+        cartOpen: !state.cartOpen,
       };
 
     default:
